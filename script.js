fc654182ff61a1bc7c668106ea4df207659d55ff
scarlett-troncoso <scar.labussieri@gmail.com>
2023-11-27 20:38:43 +0100

    add initial tags

---
+++ script.js
@@ -0,0 +1,33 @@
+Descrizione:
+/*
+Il programma dovrà chiedere all'utente:
+  il numero di chilometri che vuole percorrere
+  e l'età del passeggero.
+
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
+Queste sono le regole:
+  il prezzo del biglietto è definito in base ai km (0.21 € al km)
+  va applicato uno sconto del 20% per i minorenni
+  va applicato uno sconto del 40% per gli over 65 (fortunelli).
+
+L'output del prezzo finale va stampata in pagina assicurandoci sia leggibile da un essere umano (massimo due cifre decimali, per indicare centesimi sul prezzo).
+*/
+
+/*
+tools:
+prompt
+const, let
+
+*/
+
+const kilometri_1 = '0.21';
+
+const user_kilometri = prompt(Quanti Kilometri vuole percorrere?)
+//chiede al utente il numero di chilometri che vuole percorrere
+    
+
+const user_eta = prompt(Quanti anni hai?)
+//chiede al utente la sua etá
+    if 
+
+
