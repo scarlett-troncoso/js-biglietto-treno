c97a35f71980a7a2bc66b9b6f863f5e1097c96fd
scarlett-troncoso <scar.labussieri@gmail.com>
2023-11-28 17:46:10 +0100

    add two else if

--- script.js
+++ script.js
@@ -2,11 +2,6 @@
 const kilometri_1 = 0.21 ;
 console.log(kilometri_1);
 
-const sconto_65 = 40 ;
-
-const sconto_minori = 20 ;
-
-
 //chiede al utente il numero di chilometri che vuole percorrere
 const user_kilometri = prompt('Quanti Kilometri vuoi percorrere?')
 console.log(user_kilometri)
@@ -16,9 +11,16 @@ console.log(user_kilometri)
 const user_eta = prompt('Quanti anni hai?')
 console.log(user_eta)
 
-const result =  user_kilometri * kilometri_1 ; //risultato biglietto genérico
 
-    if ((user_eta >= 18) && (user_eta < 65)) 
+//Variabili risultati e sconti
+const result =  user_kilometri * kilometri_1 ; //risultato biglietto genérico
+const sconto_65 = 40 / result * 100 ; // calcolo sconto over 65
+const sconto_minori = 20 / result * 100 ; // calcolo sconto minoreni
+const result_2 =  user_kilometri * kilometri_1 - sconto_65 ; //risultato over 65
+const result_3 =  user_kilometri * kilometri_1 - sconto_minori; //risultato minoreni
+
+
+    if ((user_eta >= 18) && (user_eta < 65)) {
     console.log (result); 
     
     // Stampa in pagina L'output del prezzo finale
@@ -27,28 +29,29 @@ const result =  user_kilometri * kilometri_1 ; //risultato biglietto genérico
 
     let prezzo_numeroElement = document.getElementById ('prezzo_numero')
     prezzo_numeroElement.innerHTML = result;
-    
-     
 
-    /*if (user_eta >= 65); 
-    const result_2=  user_kilometri * Number (Kilometri_1) - Number (sconto_65) ;
-    console.log (result_2); 
-    //risultato over 65
-    
-    let prezzoElement = document.getElementById ('Prezzo')
-    prezzoElement.innerHTML = 'Prezzo del Biglietto: ${result_3}';
-    // Stampa in pagina L'output del prezzo finale
+        } else if (user_eta >= 65) {
+            console.log (result_2);
 
-    if (user_eta < 18);
-    const result_3=  user_kilometri * Number (Kilometri_1) - Number (sconto_minori) ;
-    console.log (result_3);   
-    //risultato minoreni 
+            // Stampa in pagina L'output del prezzo finale
+            let prezzoElement = document.getElementById ('prezzo')
+            prezzo.innerHTML = 'Prezzo del Biglietto:', result_2;
+        
+            let prezzo_numeroElement = document.getElementById ('prezzo_numero')
+            prezzo_numeroElement.innerHTML = result_2;
+        
+        } else if (user_eta < 18) {
+            console.log (result_3);
 
-        let prezzoElement = document.getElementById ('Prezzo')
-        prezzoElement.innerHTML = 'Prezzo del Biglietto: ${result_1}';
-        // Stampa in pagina L'output del prezzo finale*/
+            // Stampa in pagina L'output del prezzo finale
+            let prezzoElement = document.getElementById ('prezzo')
+            prezzo.innerHTML = 'Prezzo del Biglietto:', result_3;
+        
+            let prezzo_numeroElement = document.getElementById ('prezzo_numero')
+            prezzo_numeroElement.innerHTML = result_3;
+        }
 
-
+            
 /*
 Descrizione:
 Il programma dovrà chiedere all'utente:
