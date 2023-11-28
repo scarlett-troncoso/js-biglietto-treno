0c508f331197fe542ea88ab8da5f6414c614f564
scarlett-troncoso <scar.labussieri@gmail.com>
2023-11-28 16:54:59 +0100

    add variables and const

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
-Descrizione:
+
+const kilometri_1 = 0.21 ;
+console.log(kilometri_1);
+
+const sconto_65 = 40 ;
+
+const sconto_minori = 20 ;
+
+
+//chiede al utente il numero di chilometri che vuole percorrere
+const user_kilometri = prompt('Quanti Kilometri vuoi percorrere?')
+console.log(user_kilometri)
+
+
+//chiede al utente la sua etá
+const user_eta = prompt('Quanti anni hai?')
+console.log(user_eta)
+
+const result =  user_kilometri * kilometri_1 ; //risultato biglietto genérico
+
+    if ((user_eta >= 18) && (user_eta < 65)) 
+    console.log (result); 
+    
+    // Stampa in pagina L'output del prezzo finale
+    let prezzoElement = document.getElementById ('prezzo')
+    prezzo.innerHTML = 'Prezzo del Biglietto:', result;
+
+    let prezzo_numeroElement = document.getElementById ('prezzo_numero')
+    prezzo_numeroElement.innerHTML = result;
+    
+     
+
+    /*if (user_eta >= 65); 
+    const result_2=  user_kilometri * Number (Kilometri_1) - Number (sconto_65) ;
+    console.log (result_2); 
+    //risultato over 65
+    
+    let prezzoElement = document.getElementById ('Prezzo')
+    prezzoElement.innerHTML = 'Prezzo del Biglietto: ${result_3}';
+    // Stampa in pagina L'output del prezzo finale
+
+    if (user_eta < 18);
+    const result_3=  user_kilometri * Number (Kilometri_1) - Number (sconto_minori) ;
+    console.log (result_3);   
+    //risultato minoreni 
+
+        let prezzoElement = document.getElementById ('Prezzo')
+        prezzoElement.innerHTML = 'Prezzo del Biglietto: ${result_1}';
+        // Stampa in pagina L'output del prezzo finale*/
+
+
 /*
+Descrizione:
 Il programma dovrà chiedere all'utente:
   il numero di chilometri che vuole percorrere
   e l'età del passeggero.
@@ -20,14 +71,8 @@ const, let
 
 */
 
-const kilometri_1 = '0.21';
 
-const user_kilometri = prompt(Quanti Kilometri vuole percorrere?)
-//chiede al utente il numero di chilometri che vuole percorrere
-    
 
-const user_eta = prompt(Quanti anni hai?)
-//chiede al utente la sua etá
-    if 
+
 
 
